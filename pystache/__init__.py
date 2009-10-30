729b2e2cd2652450244f5ad129c3d258f8aa7319
Eric Florenzano <floguy@gmail.com>
2009-10-30 13:12:27 -0700

    Made the same changes to the module-level render method.

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -1,8 +1,7 @@
 from pystache.template import Template
 from pystache.view import View
 
-def render(template, context={}, **kwargs):
-    context = context.copy()
-    for key in kwargs:
-        context[key] = kwargs[key]
+def render(template, context=None, **kwargs):
+    context = context and context.copy() or {}
+    context.update(kwargs)
     return Template(template, context).render()
