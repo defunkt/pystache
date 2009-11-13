0f794a1dc23c733fe78ffa41a63b0fcf3e965f9a
Chris Wanstrath <chris@ozmm.org>
2009-11-12 22:29:37 -0800

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Pystache',
+      version='1.0',
+      description='Mustache for Python',
+      author='Chris Wanstrath',
+      author_email='chris@ozmm.org',
+      url='http://github.com/defunkt/pystache',
+      packages=['pystache'],
+      license='MIT'
+     )
