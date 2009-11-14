613a8cd2b52e29cf314fc7e70a86092c824d9123
Chris Wanstrath <chris@ozmm.org>
2009-11-13 16:21:23 -0800

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pystache',
-      version='0.1.0',
+      version='0.1.1',
       description='Mustache for Python',
       author='Chris Wanstrath',
       author_email='chris@ozmm.org',
