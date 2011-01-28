4b589295895acf33dd45dd3c074a19b5fa996727
Carl Whittaker <carl.whittaker@hogarthww.com>
2011-01-28 11:06:00 +0000

    Adding test for flattened dictionary

--- pystache/view.py
+++ pystache/view.py
@@ -29,10 +29,10 @@ class View(object):
     
     def __init__(self, template=None, context=None, **kwargs):
         self.template = template
-        self.context = context or {}
-        self.context.update(**kwargs)
+        context = context or {}
+        context.update(**kwargs)
 
-        self.context_list = [self.context]
+        self.context_list = [context]
         
     def get(self, attr, default=None):
         attr = get_or_attr(self.context_list, attr, getattr(self, attr, default))
@@ -64,6 +64,13 @@ class View(object):
 
         return re.sub('[A-Z]', repl, template_name)[1:]
 
+    def _get_context(self):
+        context = {}
+        for item in self.context_list:
+            if hasattr(item, 'keys') and hasattr(item, '__getitem__'):
+                context.update(item)
+        return context
+
     def render(self, encoding=None):
         return Template(self.get_template(self.template_name), self).render(encoding=encoding)
 
@@ -74,8 +81,14 @@ class View(object):
         val = self.get(attr, None)
 
         if not val and val is not 0:
-            raise KeyError("No such key '%s'." % attr)
+            raise KeyError("Key '%s' does not exist in View" % attr)
         return val
-    
+
+    def __getattr__(self, attr):
+        if attr == 'context':
+            return self._get_context()
+
+        raise AttributeError("Attribute '%s' does not exist in View" % attr)
+
     def __str__(self):
         return self.render()
