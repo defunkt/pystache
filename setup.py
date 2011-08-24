6886173ce0694478b31b4e7acdb0bf0368167db2
Kenneth Reitz <me@kennethreitz.com>
2011-08-23 21:27:28 -0400

    consistent string types

--- setup.py
+++ setup.py
@@ -32,11 +32,11 @@ setup(
     packages=['pystache'],
     license='MIT',
     classifiers = (
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6"
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6'
     )
 )
 
