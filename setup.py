43d504b5dbdb043cdfe9a87ffb4b3b239c130ab8
Kenneth Reitz <me@kennethreitz.com>
2011-08-23 21:25:28 -0400

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -8,29 +9,32 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
 def publish():
     """Publish to Pypi"""
-    os.system("python setup.py sdist upload")
+    os.system('python setup.py sdist upload')
 
-if sys.argv[-1] == "publish":
+if 'publish' in sys.argv:
     publish()
     sys.exit()
 
-setup(name='pystache',
-      version='0.3.1',
-      description='Mustache for Python',
-      long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
-      author='Chris Wanstrath',
-      author_email='chris@ozmm.org',
-      url='http://github.com/defunkt/pystache',
-      packages=['pystache'],
-      license='MIT',
-      classifiers = ( 
+
+setup(
+    name='pystache',
+    version='0.3.1',
+    description='Mustache for Python',
+    long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    author='Chris Wanstrath',
+    author_email='chris@ozmm.org',
+    url='http://github.com/defunkt/pystache',
+    packages=['pystache'],
+    license='MIT',
+    classifiers = (
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.5",
-        "Programming Language :: Python :: 2.6",
-        )
-     )
+        "Programming Language :: Python :: 2.6"
+    )
+)
 
