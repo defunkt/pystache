84f080421f3b244bc8427469f8fee1072e21954b
Chris Jerdonek <chris.jerdonek@gmail.com>
2011-12-18 12:28:17 -0800

    Added some unit tests for Template.render().

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,6 +5,7 @@ Unit tests of template.py.
 
 """
 
+import codecs
 import unittest
 
 from pystache.template import Template
@@ -30,4 +31,26 @@ class TemplateTestCase(unittest.TestCase):
         context = {}
         template = Template(context=context, foo="bar")
         self.assertEquals(context, {})
+
+    def test_render__unicode(self):
+        template = Template(u'foo')
+        actual = template.render()
+        self.assertTrue(isinstance(actual, unicode))
+        self.assertEquals(actual, u'foo')
+
+    def test_render__str(self):
+        template = Template('foo')
+        actual = template.render()
+        self.assertTrue(isinstance(actual, str))
+        self.assertEquals(actual, 'foo')
+
+    def test_render__context(self):
+        template = Template('Hi {{person}}', {'person': 'Mom'})
+        self.assertEquals(template.render(), 'Hi Mom')
+
+    def test_render__output_encoding(self):
+        template = Template(u'Poincaré')
+        actual = template.render('utf-8')
+        self.assertTrue(isinstance(actual, str))
+        self.assertEquals(actual, 'Poincaré')
 
