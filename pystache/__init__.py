8ecb7b48515154794de46bfc8fb5cde636951665
Brice Fernandes <bfer@cbnl.com>
2012-02-14 16:10:13 +0000

    Allow template path to be passed to render function

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -2,7 +2,8 @@ from pystache.template import Template
 from pystache.view import View
 from pystache.loader import Loader
 
-def render(template, context=None, **kwargs):
+def render(template, context=None, path=None, **kwargs):
     context = context and context.copy() or {}
     context.update(kwargs)
-    return Template(template, context).render()
+    view = View(context=context, path=path)
+    return Template(template, view).render()
