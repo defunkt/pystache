ce5cc9748c8929a045d3993790bd01ee51fc7020
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-03-31 09:49:23 -0700

    Renamed template.py to parsed.py.

--- pystache/parser.py
+++ pystache/parser.py
@@ -9,7 +9,7 @@ This module is only meant for internal use by the renderengine module.
 
 import re
 
-from template import ParsedTemplate
+from parsed import ParsedTemplate
 
 
 DEFAULT_DELIMITERS = ('{{', '}}')
