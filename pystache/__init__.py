567c41443e8fb46da0dea257600429f0711b023b
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-15 17:24:57 -0700

    Added a TODO to pystache/__init__.py about "from pystache import *".

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -1,2 +1,6 @@
 # We keep all initialization code in a separate module.
 from pystache.init import *
+
+# TODO: make sure that "from pystache import *" exposes only the following:
+#   ['__version__', 'render', 'Renderer', 'TemplateSpec'] 
+#   and add a unit test for this.
