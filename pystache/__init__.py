2a6ed788455db12fe9a103116b33dbd39240622e
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-16 08:08:19 -0500

    Added a TODO to the package __init__ about importing from pystache.init.

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -1,4 +1,6 @@
 # We keep all initialization code in a separate module.
+# TODO: consider doing something like this instead:
+#   from pystache.init import __version__, render, Renderer, TemplateSpec
 from pystache.init import *
 
 # TODO: make sure that "from pystache import *" exposes only the following:
