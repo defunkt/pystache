328b018b9ac5bd6695a477d29989d0946881dcd7
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-16 20:50:23 -0500

    Updated pystache/commands/test.py to use the new test-discovery code.

--- pystache/commands/test.py
+++ pystache/commands/test.py
@@ -7,20 +7,14 @@ This module provides a command to test pystache (unit tests, doctests, etc).
 
 import sys
 
-# TODO: make nose unnecessary.
-import nose
-
 import pystache
+from pystache.tests.main import Tester
 
 
-def main(sys_argv):
-    # This does not work with the --with-doctest flag yet because of the
-    # following issue:
-    #   https://github.com/nose-devs/nose/issues/383
-    # TODO: change module keyword argument to defaultTest keyword argument:
-    #   http://readthedocs.org/docs/nose/en/latest/api/core.html#module-nose.core
-    nose.main(module=pystache)
+def main(sys_argv=sys.argv):
+    tester = Tester()
+    tester.run_tests(package=pystache, sys_argv=sys_argv)
 
 
 if __name__=='__main__':
-    main(sys.argv)
+    main()
