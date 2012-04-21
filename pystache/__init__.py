88c5c3df172c8e222582f6dfd66f4b341ac2dd81
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-21 08:24:11 -0700

    Deleted a trailing space.

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -4,5 +4,5 @@
 from pystache.init import *
 
 # TODO: make sure that "from pystache import *" exposes only the following:
-#   ['__version__', 'render', 'Renderer', 'TemplateSpec'] 
+#   ['__version__', 'render', 'Renderer', 'TemplateSpec']
 #   and add a unit test for this.
