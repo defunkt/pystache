e05966ee30144a624f88f94ac615e42ff4c5a8ac
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-23 13:22:03 -0700

    Removed a TODO that has already been addressed.

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -5,7 +5,7 @@ TODO: add a docstring.
 """
 
 # We keep all initialization code in a separate module.
-# TODO: consider doing something like this instead:
+
 from pystache.init import render, Renderer, TemplateSpec
 
 __all__ = ['render', 'Renderer', 'TemplateSpec']
