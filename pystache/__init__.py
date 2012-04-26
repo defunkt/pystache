cc262abf19cd90e34390d5ddb5db30d6f04620fa
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-04-25 22:49:19 -0700

    Bump version from 0.5.1-rc to 0.5.1 prior to release to PyPI.

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -10,4 +10,4 @@ from pystache.init import render, Renderer, TemplateSpec
 
 __all__ = ['render', 'Renderer', 'TemplateSpec']
 
-__version__ = '0.5.1-rc'  # Also change in setup.py.
+__version__ = '0.5.1'  # Also change in setup.py.
