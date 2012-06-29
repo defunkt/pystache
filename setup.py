88516160909af9ce0c66d05ef1761946335641be
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-06-29 12:39:42 -0700

    Add PyPy to Trove classifier in setup().

--- setup.py
+++ setup.py
@@ -123,6 +123,7 @@ CLASSIFIERS = (
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.1',
     'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: Implementation :: PyPy',
 )
 
 
