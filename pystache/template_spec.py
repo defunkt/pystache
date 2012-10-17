e255b92589000c2d485d35f9008b78e0313b4374
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-10-16 17:06:19 -0700

    Reorder TemplateSpec attributes and add to docstring.

--- pystache/template_spec.py
+++ pystache/template_spec.py
@@ -4,12 +4,11 @@
 Provides a class to customize template information on a per-view basis.
 
 To customize template properties for a particular view, create that view
-from a class that subclasses TemplateSpec.  The "Spec" in TemplateSpec
-stands for template information that is "special" or "specified".
+from a class that subclasses TemplateSpec.  The "spec" in TemplateSpec
+stands for "special" or "specified" template information.
 
 """
 
-# TODO: finish the class docstring.
 class TemplateSpec(object):
 
     """
@@ -28,20 +27,24 @@ class TemplateSpec(object):
 
       template: the template as a string.
 
-      template_rel_path: the path to the template file, relative to the
-        directory containing the module defining the class.
-
-      template_rel_directory: the directory containing the template file, relative
-        to the directory containing the module defining the class.
+      template_encoding: the encoding used by the template.
 
       template_extension: the template file extension.  Defaults to "mustache".
         Pass False for no extension (i.e. extensionless template files).
 
+      template_name: the name of the template.
+
+      template_rel_directory: the directory containing the template file,
+        relative to the directory containing the module defining the class.
+
+      template_rel_path: the path to the template file, relative to the
+        directory containing the module defining the class.
+
     """
 
     template = None
-    template_rel_path = None
-    template_rel_directory = None
-    template_name = None
-    template_extension = None
     template_encoding = None
+    template_extension = None
+    template_name = None
+    template_rel_directory = None
+    template_rel_path = None
