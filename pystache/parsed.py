5b00b6cd392a1f5f317cc18893198bb5d62c565d
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-10-19 16:35:36 -0700

    Rename variable name from "val" to "node".

--- pystache/parsed.py
+++ pystache/parsed.py
@@ -40,10 +40,10 @@ class ParsedTemplate(object):
 
         """
         # We avoid use of the ternary operator for Python 2.4 support.
-        def get_unicode(val):
-            if type(val) is unicode:
-                return val
-            return val.render(engine, context)
+        def get_unicode(node):
+            if type(node) is unicode:
+                return node
+            return node.render(engine, context)
         parts = map(get_unicode, self._parse_tree)
         s = ''.join(parts)
 
