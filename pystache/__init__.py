0810269750e16364374cbe268c997675c02826f7
Bradley Young <young.bradley@gmail.com>
2014-12-15 16:53:30 -0500

    updating version number

--- pystache/__init__.py
+++ pystache/__init__.py
@@ -10,4 +10,4 @@ from pystache.init import parse, render, Renderer, TemplateSpec
 
 __all__ = ['parse', 'render', 'Renderer', 'TemplateSpec']
 
-__version__ = '0.5.4'  # Also change in setup.py.
+__version__ = '0.5.4a'  # Also change in setup.py.
