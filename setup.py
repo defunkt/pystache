df62fd9e5398df9627482c48f63d7261f9132ddf
cclauss <cclauss@bluewin.ch>
2017-11-12 20:36:39 +0100

    Add Python 3.6 to setup.py

--- setup.py
+++ setup.py
@@ -141,6 +141,7 @@ CLASSIFIERS = (
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: Implementation :: PyPy',
 )
 
